6f571ccb4aeaaa084949bb9166c73f3431446a26
samuelrodriguez <samuell.rodriguezz99@gmail.com>
2026-01-02 11:55:44 -0300

    Rate: 8 to 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Movie myMovie1 = new Movie();
+        myMovie1.name = "Forrest Gump";
+        myMovie1.yearOfRelease = 1994;
+        myMovie1.durationInMinutes = 142;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        myMovie1.displaysTechnicalSheet();
+
+        myMovie1.rate(7);
+        myMovie1.rate(5);
+        myMovie1.rate(4);
+        System.out.println(myMovie1.sum_Of_Assessments);
+        System.out.println(myMovie1.totalReviews);
+        System.out.println(myMovie1.averageRating());
     }
 }
