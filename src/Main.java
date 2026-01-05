f96661696c593a8919109e7015631c6f6d94211e
samuelrodriguez <samuell.rodriguezz99@gmail.com>
2026-01-04 22:04:19 -0300

    Set, Get and private

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
+import br.com.alura.screenmatch.modelos.Movie;
+
 public class Main {
     public static void main(String[] args) {
+        // Preenchendo os atributos
         Movie myMovie1 = new Movie();
-        myMovie1.name = "Forrest Gump";
-        myMovie1.yearOfRelease = 1994;
-        myMovie1.durationInMinutes = 142;
+        myMovie1.setName("Forrest Gump");
+        myMovie1.setYearOfRelease(1994);
+        myMovie1.setDurationInMinutes(142);
 
+       // Chamando métodos da classe Filme
         myMovie1.displaysTechnicalSheet();
 
+        // Avaliações
         myMovie1.rate(4);
         myMovie1.rate(5);
         myMovie1.rate(4);
-        System.out.println(myMovie1.sum_Of_Assessments);
-        System.out.println(myMovie1.totalReviews);
-        System.out.println(myMovie1.averageRating());
+
+        //Exibindo a média das avaliações
+        System.out.println("Total of Reviews: " + myMovie1.getTotalReviews());
+        //System.out.println(myMovie1.averageRating());
     }
 }
---
+++ src/br/com/alura/screenmatch/modelos/Movie.java
@@ -0,0 +1,48 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Movie {
+    // ATRIBUTOS (privados) DA CLASSE MOVIE
+    private String name;
+    private int yearOfRelease;
+    private boolean withinThePlan;
+    private double sum_Of_Assessments;
+    private int totalReviews;
+    private int durationInMinutes;
+
+    public int getTotalReviews(){
+        return totalReviews;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public void setWithinThePlan(boolean withinThePlan) {
+        this.withinThePlan = withinThePlan;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+
+    // MÉTODO PARA EXIBIR A FICHA TÉCNICA
+    public void displaysTechnicalSheet(){
+        System.out.println("br.com.alura.screenmatch.modelos.Movie Name: " + name);
+        System.out.println("Year of Release: " + yearOfRelease);
+    }
+    // MÉTODO PARA REGISTRAR UMA AVALIAÇÃO
+    public void rate(double note){
+        sum_Of_Assessments += note;
+        totalReviews++;
+
+    }
+    // MÉTODO PARA CALCULAR A MÉDIA DAS AVALIAÇÕES
+    public double averageRating(){
+        return sum_Of_Assessments / totalReviews;
+    }
+}
